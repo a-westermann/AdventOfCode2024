ec4b772ab1fbd77b1dadbd32f2b2c4b165b5ed4e
Andrew <a.westermann.19@gmail.com>
2024-12-26 09:49:33 -0600

    day 4 part 1 work

---
+++ 04.py
@@ -0,0 +1,42 @@
+def find_xmas(p_grid: [[]], direction: (int, int), x: int, y: int) -> bool:
+    for i in range(1, 4):
+        x_check, y_check = x + direction[0] * i, y + direction[1] * i
+        if y_check < 0 or y_check >= len(p_grid) or x_check < 0 or x_check >= len(p_grid[0]):
+            continue
+        letter = p_grid[y + direction[1] * i][x + direction[0] * i]
+        print(f'    x = {x_check}        y = {y_check}     letter = {letter}')
+        if letter != 'MAS'[i-1]:
+            return False
+
+    return True
+
+
+with open('puzzle_04.txt') as f:
+    puzzle = f.readlines()
+
+grid = []
+for row in puzzle:
+    grid.append([])
+    for c in row.strip():
+        grid[-1].append(c)
+
+dirs = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+xmas_grid = list(grid)
+for y in range(len(xmas_grid)):
+    for x in range(y):
+        xmas_grid[y][x] = ''
+
+result = 0
+for y, row in enumerate(grid):
+    for x, cell in enumerate(row):
+        if cell != 'X':
+            continue
+
+        for d in dirs:
+            if find_xmas(grid, d, x, y):
+                result += 1
+                xmas_grid[y][x] = grid[y][x]
+
+print(result)
+for row in xmas_grid:
+    print(row)
